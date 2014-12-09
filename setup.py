65eae715b9e78a981eda0a902910f935ab44bf50
tyler46 <mail.doctor46@gmail.com>
2014-12-09 16:23:06 +0200

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 setup(name='cenviron',
-      version='2.0',
+      version='3.0',
       description='A simple get/set environment variable example',
+      author="Tyler Durden",
+      author_email="mail.doctor46@gmail.com",
+      url="https://github.com/tyler46/python-c-extension-demo",
       py_modules=['envattr'],
       ext_modules=[Extension('cenviron', ['src/cenviron.c'])])
